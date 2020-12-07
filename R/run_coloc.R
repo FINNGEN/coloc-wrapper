ffad7ef0b3b7778d3ead8f4dd36a82b14da0ae8e
Sina Rueger <sina.rueeger@gmail.com>
2020-12-07 15:53:05 +0000

    add proper locuszoom filename + title

--- R/run_coloc.R
+++ R/run_coloc.R
@@ -36,9 +36,8 @@ split_string <- function(string, n=40) {
 #' @param filename the name of the output file 
 #' @param coloc coloc results (h0-h4 columns)
 
-locuscompare <- function(df, gene, filename, coloc = NULL) {
+locuscompare <- function(df, gene, filename, title, coloc = NULL) {
 
-    filename <- sapply(strsplit(filename, ".", fixed = TRUE), function(x) x[1])
 
     if (!(any(names(df) %in% "pvalues.gwas"))) {
         stop("GWAS pvalue column is needed for the locuscompare plot, but missing from the data.")
@@ -50,7 +49,7 @@ locuscompare <- function(df, gene, filename, coloc = NULL) {
 
     plot <- ggplot2::ggplot(data = df, aes(x = -log10(pvalues.gwas), y = -log10(pvalues.eqtl))) + geom_point(size = 0.6) + geom_abline(color = "black", linetype = 3) + 
         geom_smooth(method = "lm", se = FALSE, color = "black", size = 0.5) + theme_light() + #coord_fixed(ratio = 1) +
-        labs(title = split_string(basename(filename)), subtitle = gene, x = "GWAS -log10(P)", y = "eQTL -log10(P)") + 
+        labs(title = title, subtitle = gene, x = "GWAS -log10(P)", y = "eQTL -log10(P)") + 
         theme(axis.text.x = element_text(size = 7), axis.text.y = element_text(size = 7), axis.text = element_text(size = 7), plot.title = element_text(size = 12)) + 
         geom_rug()
     
@@ -112,7 +111,8 @@ run_coloc <- function(eqtl_data, gwas_data, out = NULL, p1 = 1e-4, p2 = 1e-4, p1
     gwas_info = list(type = "cc", s = NA, N = NA), 
     gwas_header = c(varid = "rsids", pvalues = "pval", MAF = "maf"),
     eqtl_header = c(varid = "rsid", pvalues = "pvalue", MAF = "maf", gene_id = "gene_id"),
-    locuscompare_thresh =  1) {
+    locuscompare_thresh =  1, 
+    locuscompare_title = out) {
     
     ## check if files empty --------------------
     if (file.info(gwas_data)$size == 0) {
@@ -197,7 +197,7 @@ run_coloc <- function(eqtl_data, gwas_data, out = NULL, p1 = 1e-4, p2 = 1e-4, p1
                     res <- as.data.frame(t(res$summary))
                                         
                     if (res$PP.H4.abf > locuscompare_thresh) {
-                        locuscompare(df = df_sub, gene = x, filename = out, coloc = res)
+                        locuscompare(df = df_sub, gene = x, filename = out, title = locuscompare_title, coloc = res)
                     }
                     
                  
--- extdata/step2_run_coloc.R
+++ extdata/step2_run_coloc.R
@@ -15,8 +15,9 @@ option_list <- list(
     optparse::make_option(c("--header_gwas"), type="character", default="c(varid = 'rsids', pvalues = 'pval', MAF = 'maf')", help="Header of the GWAS file, named vector in quotes"),
     optparse::make_option(c("--info_gwas"), type="character", default="list(type = 'cc', s = 11006/117692, N  = 11006 + 117692)", help="Options for gwas datalist"),
     optparse::make_option(c("--info_eqtl"), type="character", default="list(type = 'quant', sdY = 1, N = 491)", help="Options for eqtl datalist"),    
-    optparse::make_option(c("--locuscompare_thresh"), type="double", default=0.7, help="Which genes to plot")  
-
+    optparse::make_option(c("--locuscompare_thresh"), type="double", default=0.7, help="Which genes to plot"),
+    optparse::make_option(c("--locuscompare_title"), type="character", default='title', help="Title for locuszoomplot")  
+  
 )
 
 opt <- optparse::parse_args(optparse::OptionParser(option_list=option_list))
